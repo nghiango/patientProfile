366baa236a800895e8de0454e0c026c0ae62de72
nghia.ngo <nghia.ngo@ubitec.ch>
2018-06-20 14:55:28 +0700

    Update allergic controller

--- src/main/java/eiu/edu/final_project/controller/AllergicController.java
+++ src/main/java/eiu/edu/final_project/controller/AllergicController.java
@@ -3,10 +3,12 @@ package eiu.edu.final_project.controller;
 import eiu.edu.final_project.domain.Allergic;
 import eiu.edu.final_project.repository.IAllergicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/allergic")
@@ -20,5 +22,12 @@ public class AllergicController {
         return iAllergicRepository.findAll();
     }
 
-    
+    @RequestMapping(value = "/{id}")
+    public Allergic getAllergic(@PathVariable String id){
+        Optional<Allergic> allergic = iAllergicRepository.findById(id);
+        if (allergic.isPresent()){
+            return allergic.get();
+        }
+        return new Allergic();
+    }
 }
